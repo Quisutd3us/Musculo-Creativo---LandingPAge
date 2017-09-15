cf0b083924a1ba8f14f78648a1a5ba89a14f8227
David Salinas FrontEnd Dev/ <dnarino@gmail.com>
2017-09-15 08:49:45 -0500

    animacion:optimizando scroll

--- js/misScripts.js
+++ js/misScripts.js
@@ -1,4 +1,9 @@
 
 // Initialize Smooth Scroll
 
-var scroll = new SmoothScroll('a[href*="#"]');
+var scroll = new SmoothScroll('a[href*="#"]',{
+    speed: 1500, // Integer. How fast to complete the scroll in milliseconds
+	offset: 50, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+	easing: 'easeInOutCubic', // Easing pattern to use
+	customEasing: function (time) {}, // Function. Custom easing pattern
+});
